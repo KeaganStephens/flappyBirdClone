29e3d30497ae2b0f26e742b769f9c6276cf3f83c
Stephens <Keagan.stephens@icloud.com>
2023-11-09 06:34:15 +0200

    added flying function(does not work)

--- app/app.component.ts
+++ app/app.component.ts
@@ -24,11 +24,13 @@ export class AppComponent implements OnInit {
       }
 
       console.log(this.birdHeight)
+      console.log(this.viewportHeight  - this.birdHeight - 50)
+      console.log("----")
       if(this.start != undefined && this.bird != undefined){
         console.log(this.start)
         this.start += 50 
         this.bird.style.top = (this.start + 50) + 'px'
-        if(this.start <= lowestPoint - this.birdHeight){
+        if(this.start <= lowestPoint - (this.birdHeight * 2) - 50){
           this.birdFalling(1, lowestPoint)
         }
       }else{
@@ -38,11 +40,27 @@ export class AppComponent implements OnInit {
       ,1000 * adjust)
   }
 
+  birdFlying(highestPoint : number){
+    console.log("1")
+    while(this.start != undefined){
+      continue
+    }
+    if(this.start != undefined && this.bird != undefined){
+      this.bird.style.top = (this.start - 50) + 'px'
+    }
+  }
+
 
   ngOnInit(){
-    this.birdFalling(1, this.viewportHeight)
+    // this.birdFalling(1, this.viewportHeight)
     console.log(this.viewportHeight)
-    
+    while(this.bird == undefined){
+      this.bird = document.getElementById('flappyBird')
+    }
+    if(this.bird != undefined){
+      console.log("hello")
+      this.bird.addEventListener('click',() => this.birdFlying)
+    }
 }
 
   

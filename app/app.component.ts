76ce2acc732bfe63cc743b2e67d04f9eb7b38f63
Stephens <Keagan.stephens@icloud.com>
2023-11-11 07:23:56 +0200

    added pillars that move

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,57 +9,98 @@ export class AppComponent implements OnInit {
   title = 'flappyBirdClone';
 
   bird = document.getElementById('flappyBird')
-  start = this.bird?.offsetTop
+  birdStart = this.bird?.offsetTop
+  pillarTop = document.getElementById('upperPillar')
+  pillarBottom = document.getElementById('lowerPillar')
   viewportHeight = window.innerHeight
   birdHeight = this.bird?.clientHeight
+  moveDownDistance = 1
+  moveUpDistance = 50
+  pillarMoveLeft = 1
+  pillarStart = this.pillarBottom?.offsetLeft
 
   birdFalling(adjust : number, lowestPoint : number){
 
     setTimeout( () => {
        
-      while(this.start == undefined || this.bird == undefined || this.birdHeight == undefined ){
+      while(this.birdStart == undefined || this.bird == undefined || this.birdHeight == undefined ){
         this.bird = document.getElementById('flappyBird')
-        this.start = this.bird?.offsetTop
+        this.birdStart = this.bird?.offsetTop
         this.birdHeight = this.bird?.clientHeight
       }
 
-      console.log(this.birdHeight)
-      console.log(this.viewportHeight  - this.birdHeight - 50)
-      console.log("----")
-      if(this.start != undefined && this.bird != undefined){
-        console.log(this.start)
-        this.start += 50 
-        this.bird.style.top = (this.start + 50) + 'px'
-        if(this.start <= lowestPoint - (this.birdHeight * 2) - 50){
+      // console.log(this.birdHeight)
+      // console.log(this.viewportHeight  - this.birdHeight - this.moveDownDistance)
+      // console.log("----")
+      if(this.birdStart != undefined && this.bird != undefined){
+        console.log(this.birdStart)
+        this.bird.style.top = (this.birdStart + this.moveDownDistance) + 'px'
+        this.birdStart += this.moveDownDistance
+        if(this.birdStart <= lowestPoint - (this.birdHeight * 2) - this.moveDownDistance){
           this.birdFalling(1, lowestPoint)
         }
       }else{
         console.log('umm')
       }
     }
-      ,1000 * adjust)
+      ,10 * adjust)
   }
 
   birdFlying(highestPoint : number){
     console.log("1")
-    while(this.start != undefined){
-      continue
+    while(this.birdStart == undefined || this.bird == undefined){
+      this.birdStart = this.bird?.offsetTop
+      this.bird = document.getElementById('flappyBird')
     }
-    if(this.start != undefined && this.bird != undefined){
-      this.bird.style.top = (this.start - 50) + 'px'
+    if(this.birdStart != undefined && this.bird != undefined){
+      this.bird.style.top = (this.birdStart - this.moveUpDistance) + 'px'
+      this.birdStart -= this.moveUpDistance 
     }
   }
 
+  pillarsMoving(adjust : number, furthersPoint : number){
+
+    setTimeout( () => {
+       
+      while(this.pillarBottom == undefined || this.pillarTop == undefined || this.pillarStart == undefined){
+        this.pillarTop = document.getElementById('upperPillar')
+        this.pillarBottom = document.getElementById('lowerPillar')
+        this.pillarStart = this.pillarBottom?.offsetLeft
+        console.log('#p' + this.pillarStart)
+      }
+
+      console.log(this.pillarBottom)
+      // console.log(this.viewportHeight  - this.birdHeight - this.moveDownDistance)
+      console.log("----")
+      if(this.pillarBottom != undefined && this.pillarTop!= undefined && this.pillarStart != undefined){
+        console.log(this.birdStart)
+        this.pillarTop.style.left = (this.pillarStart - this.pillarMoveLeft) + 'px'
+        this.pillarBottom.style.left = (this.pillarStart - this.pillarMoveLeft) + 'px'
+        console.log("----" + this.pillarBottom.style.right)
+        console.log('#p' + this.pillarStart)
+        this.pillarStart -= this.pillarMoveLeft
+        if(this.pillarStart >= furthersPoint ){
+          this.pillarsMoving(1, furthersPoint)
+        }
+      }else{
+        console.log('umm')
+      }
+    }
+      ,10 * adjust)
+  }
 
   ngOnInit(){
-    // this.birdFalling(1, this.viewportHeight)
+    let container = document.getElementById('container')
+
+    this.birdFalling(1, this.viewportHeight)
+    this.pillarsMoving(1, 0)
     console.log(this.viewportHeight)
     while(this.bird == undefined){
       this.bird = document.getElementById('flappyBird')
     }
-    if(this.bird != undefined){
+    if(container != undefined){
       console.log("hello")
-      this.bird.addEventListener('click',() => this.birdFlying)
+      container.addEventListener('click',() => this.birdFlying(1))
     }
 }
 
